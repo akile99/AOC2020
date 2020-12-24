5473b473e5cf75b15a76c0621cc3f98178368c49
akile <akile99@gmail.com>
2020-12-23 23:47:58 -0500

    day1

---
+++ Day1/day1.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const test = 'test.txt'
+const input = 'input.txt'
+
+question1 = (text, file) => {
+	fs.readFile(`./${file}`, (err, data) => {
+	console.time('expense-time');
+	if (err) {
+			console.log('eerrerrrrooorrr');
+		}
+	
+	const expenseReport = data.toString();
+	const expenseReportArray = expenseReport.split('\n');
+	// loop over all the line in the file
+	console.log(`The answer is to ${text} is ${partone(expenseReportArray)}`);
+	console.log(`The second part ${text} is ${parttwo(expenseReportArray)}`);
+	console.timeEnd('expense-time');
+	})
+}
+	
+
+checkSum = (a, b) => Number(a) + Number(b);
+
+multiplyNums = (a, b) => (Number(a) * Number(b));
+
+partone = (array) => {
+	let first = 0;
+	let second = 0;
+	array.some(a => {
+		array.some(b => {
+			if(checkSum(a, b) === 2020) {
+				first = a;
+				second = b;
+			};
+		})
+	})
+	return multiplyNums(first,second)
+}
+
+parttwo = (array) => {
+	let first = 0;
+	let second = 0;
+	let third = 0;
+	array.some(a => {
+		array.some(b => {
+			array.some(c => {
+				if(checkSum(checkSum(a,b),c) === 2020) {
+					first = a;
+					second = b;
+					third = c;
+				}
+			})
+		})
+	})
+	return multiplyNums(multiplyNums(first,second),third)
+}
+
+	// multiply the two numbers that equal 2020
+//question1("test", test)
+question1("input", input)
+
